9a6843a7bd5c61f361ef1a74de300c4da3f0648f
Matthew Stolley <mstolley@crocs.com>
2025-04-08 13:28:01 -0600

    add and use Accordion component on Home.

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,14 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import type { Blend, Spice } from '../types';
 import { Header } from '../components/Header';
+import { Accordion } from '../components/Accordion';
 
 function Home() {
     const [spices, setSpices] = useState<Spice[]>([]);
     const [blends, setBlends] = useState<Blend[]>([]);
-    const [searchString, updateSearchString] = useState('');
-    const [isSpicesOpen, setIsSpicesOpen] = useState(false);
-    const [isBlendsOpen, setIsBlendsOpen] = useState(false);
 
     useEffect(() => {
         async function fetchSpices() {
@@ -30,64 +27,8 @@ function Home() {
     return (
         <div className='m-1.5 md:m-2.5 lg:m-5 text-gray-800'>
             <Header text='Spice and Blend List' />
-            <div>
-                <h4
-                    className='text-2xl font-bold cursor-pointer mb-2.5 hover:text-gray-600 transition-colors delay-100 duration-300 ease-in-out'
-                    onClick={() => setIsSpicesOpen(!isSpicesOpen)}
-                >
-                    Spice List
-                </h4>
-                {isSpicesOpen && (
-                    <div className='mb-5 flex flex-col'>
-                        <input
-                            className='border-2 border-gray-400 rounded-md p-2.5 mb-2.5 sm:w-full md:w-1/2'
-                            value={searchString}
-                            onChange={(e) => {
-                                updateSearchString(e.target.value);
-                            }}
-                        />
-                        {spices
-                            .filter((spice) =>
-                                spice.name
-                                    .toLowerCase()
-                                    .includes(searchString.toLowerCase()),
-                            )
-                            .map((spice) => (
-                                <div key={spice.id}>
-                                    <Link to={`/spices/${spice.id}`}>
-                                        {spice.name}
-                                    </Link>
-                                </div>
-                            ))}
-                    </div>
-                )}
-            </div>
-
-            <div>
-                <h4
-                    className='text-2xl font-bold cursor-pointer mb-2.5 hover:text-gray-600 transition-colors delay-100 duration-300 ease-in-out'
-                    onClick={() => setIsBlendsOpen(!isBlendsOpen)}
-                >
-                    Blend List
-                </h4>
-                {isBlendsOpen && (
-                    <div className='mb-5 flex flex-col'>
-                        {blends
-                            .filter((blend) =>
-                                blend.name
-                                    .toLowerCase()
-                                    .includes(searchString.toLowerCase()),
-                            )
-                            .map((blend) => (
-                                <div key={blend.id}>
-                                    <Link to={`/blends/${blend.id}`}>
-                                        {blend.name}
-                                    </Link>
-                                </div>
-                            ))}
-                    </div>
-                )}
-            </div>
+            <Accordion text="Spice List" list={spices} />
+            <Accordion text="Blend List" list={blends} />
         </div>
     );
 }
