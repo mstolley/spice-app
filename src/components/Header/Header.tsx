be38702d657054460a6a27270b19c2a5ded2a595
Matthew Stolley <mstolley@crocs.com>
2025-04-09 16:19:41 -0600

    shrink header nav a bit.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,7 +24,7 @@ function Header({ text = 'Spice App' }: HeaderProps) {
           className={`mb-5 h-7 w-full`}
         >
           <Link className={`
-            rounded-md bg-gray-200 px-4 py-2 text-xs text-blue-500
+            h-7 rounded-sm bg-gray-200 px-2.5 py-1 text-xs text-blue-500
             transition-colors delay-100 duration-300 ease-in-out
             hover:text-blue-600
           `} to="/">Home</Link>
